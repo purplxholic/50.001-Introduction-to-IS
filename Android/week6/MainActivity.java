d1d2a4e5d7bddac06a5c865ccb86530b6b421078
purplxholic <zanette_cheng@mymail.sutd.edu.sg>
2016-10-31 20:48:58 +0800

    Add files via upload

---
+++ Android/week6/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.zan.firebase;
+//followed https://cloud.google.com/solutions/mobile/firebase-app-engine-android-studio
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mFirebaseDatabaseReference;
+    private FirebaseDatabase database; // to send to database
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //so that the program knows which activity to go back to
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance(); // connect to firebase
+        mFirebaseDatabaseReference = database.getReference("test");
+
+        mFirebaseDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG:", "Failed to read value.", databaseError.toException());
+            }
+        });
+
+
+        final Button button = (Button) findViewById(R.id.button);
+        final EditText text = (EditText) findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener()
+
+        {
+            public void onClick(View view) {
+
+                DatabaseReference childRed = mFirebaseDatabaseReference.push();
+                childRed.setValue(text.getText().toString());
+                Toast.makeText(MainActivity.this, "Conected to firebase!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View view){
+//                DataSnapshot datasnaphot = DataSnapshot();
+//                if (datasnaphot.getValue().toString().equals(text.getText().toString())){
+//                    Toast.makeText(MainActivity.this, "Conected to firebase!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//            );
+//    public void checky(View view) {
+//        if (true) {
+//            Toast.makeText(MainActivity.this, "Conected to firebase!", Toast.LENGTH_SHORT).show();
+//        }
+//        Toast.makeText(MainActivity.this, "Error! Not Connected.", Toast.LENGTH_SHORT).show();
+    }
+}
+
